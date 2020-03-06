35c3554e1a23c8de86e859ee310c4379c14f9282
Hugo <dragona852@gmail.com>
2020-03-06 22:47:23 +0100

    step2 notfinished

--- server/michelin.js
+++ server/michelin.js
@@ -10,8 +10,48 @@ const parse = data => {
   const $ = cheerio.load(data);
   const name = $('.section-main h2.restaurant-details__heading--title').text();
   const experience = $('#experience-section > ul > li:nth-child(2)').text();
+  const links =[];
+  
+  //find the nb of restaurants
+  var NbOfrestaurant = $('body > main > section.section-main.search-results.search-listing-result > div > div > div.search-results__count > div.d-flex.align-items-end.search-results__status > div.flex-fill > h1').text();
+  for (let i = 0; i < NbOfrestaurant.length-3; i++){
+    if (NbOfrestaurant[i]== 's' && NbOfrestaurant[i+1]== 'u' && NbOfrestaurant[i+2]== 'r' ){
+      var k=i+4
+      var nbres =NbOfrestaurant[k]
+      while (NbOfrestaurant[k]!=' '){
+        k++;
+      }
+      
+      for (let j = i+5; j < k; j++){nbres += NbOfrestaurant[j]}
+      
+      NbOfrestaurant=nbres;
+    }
+  }
+  
+  
+  $('a.link').each( function() {
+    var link = $(this).attr('href');
+    links.push({"link" : link})
+  });
+  //const price = $('a.link').attr('href');
+  return {name, experience, links, NbOfrestaurant};
+};
 
-  return {name, experience};
+const parse2 = data => {
+  /*
+  //json
+  var megajson = 
+  */
+
+  const $ = cheerio.load(data);
+  const name  =$('body > main > div.restaurant-details > div.container > div > div.col-xl-4.order-xl-8.col-lg-5.order-lg-7.restaurant-details__aside > div.restaurant-details__heading.d-lg-none > h2').text();
+  const tel = $('body > main > div.restaurant-details > div.container > div > div.col-xl-8.col-lg-7 > section:nth-child(4) > div.row > div:nth-child(1) > div > div:nth-child(1) > div > div > a').attr('href');
+  const adress = $('body > main > div.restaurant-details > div.container > div > div.col-xl-4.order-xl-8.col-lg-5.order-lg-7.restaurant-details__aside > div.restaurant-details__heading.d-lg-none > ul > li:nth-child(1)').text();
+  var prix = $('body > main > div.restaurant-details > div.container > div > div.col-xl-4.order-xl-8.col-lg-5.order-lg-7.restaurant-details__aside > div.restaurant-details__heading.d-lg-none > ul > li.restaurant-details__heading-price').text();
+  const avis = $('#experience-section > ul > li:nth-child(2)').text();
+
+  prix = prix.replace(/[^\d.-]/g, '');
+  return {name,prix,tel,adress,avis};
 };
 
 /**
@@ -31,6 +71,25 @@ module.exports.scrapeRestaurant = async url => {
 
   return null;
 };
+
+/**
+ * Scrape a given restaurant url
+ * @param  {String}  url
+ * @return {Object} restaurant
+ */
+module.exports.scrapeRestaurantsingle = async url => {
+  const response = await axios(url);
+  const {data, status} = response;
+
+  if (status >= 200 && status < 300) {
+    return parse2(data);
+  }
+
+  console.error(status);
+
+  return null;
+};
+
 
 /**
  * Get all France located Bib Gourmand restaurants
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,13 +1,46 @@
 /* eslint-disable no-console, no-process-exit */
 const michelin = require('./michelin');
+var fs = require('fs');
 
-async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/centre-val-de-loire/veuves/restaurant/l-auberge-de-la-croix-blanche') {
+async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/page/1') {
   try {
     console.log(`🕵️‍♀️  browsing ${searchLink} source`);
 
-    const restaurant = await michelin.scrapeRestaurant(searchLink);
+    //find the nb of pages
+    const nbrest = await michelin.scrapeRestaurant(searchLink);
+    const nbOfPages = Math.round(nbrest.NbOfrestaurant/20);
+    
+    //create and explore all the pages nbOfPages-13
+    var listoflinks = [];
+    var listoflinks2 = [];
+    for (let i = 1; i <= 1; i++){
+      const truc = 'https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/page/' + i
+      const restaurant = await michelin.scrapeRestaurant(truc);
+      //restaurant.links.foreach(element =>sandbox22(element) );
+      
+      listoflinks = restaurant.links;
+      listoflinks.forEach(element => listoflinks2.push( 'https://guide.michelin.com' + element.link));
 
-    console.log(restaurant);
+    }
+    /*
+    console.log(listoflinks2)
+    for (var i =0; i<listoflinks2.length;i++){
+      await sandbox22(searchLink = listoflinks2[i]);
+    }
+*/
+let data = "Learning how to write in a file.";
+fs.writeFile('Output.txt', data, (err) => { 
+      
+  // In case of a error throw err. 
+  if (err) throw err; 
+}) 
+
+
+    fs.writeFile('AllRestaurantsLinks.txt', data, function (err) {
+      if (err) throw err;
+      console.log('File is created successfully.');
+    });
+
     console.log('done');
     process.exit(0);
   } catch (e) {
@@ -16,6 +49,63 @@ async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/centre-va
   }
 }
 
-const [,, searchLink] = process.argv;
+//useless now
+//return an array with the links for michelin
+async function linksOfMichelin (searchLink = 'https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/page/1') {
+  try {
+  console.log(`🕵️‍♀️  browsing ${searchLink} source`);
 
+  const restaurant = await michelin.scrapeRestaurant(searchLink);
+  const nbOfPages = Math.round(restaurant.NbOfrestaurant/20);
+
+  var anarray = [];
+  for (let i = 1; i <= nbOfPages+1; i++){
+    const truc = 'https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/page/' + i
+    //console.log(truc)
+    anarray.push(truc);
+  }
+  console.log(anarray)
+  console.log('done');
+  return anarray; 
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+}
+}
+//= 'https://guide.michelin.com/fr/fr/occitanie/saint-savin/restaurant/les-3-faisans'
+async function sandbox22 (searchLink) {
+  try {
+    console.log(`🕵️‍♀️  browsing ${searchLink} source`);
+
+    //find the nb of pages
+    const restaurant = await michelin.scrapeRestaurantsingle(searchLink);
+    console.log('name')
+    console.log(restaurant.name)
+    console.log('prix')
+    console.log(restaurant.prix)
+    console.log('tel')
+    console.log(restaurant.tel)
+    console.log('adress')
+    console.log(restaurant.adress)
+    console.log('avis')
+    console.log(restaurant.avis)
+    /*
+    var restinjson = JSON.stringify(restaurant, null, 2);
+    fs.writeFile("restMichelin.json", restinjson);
+    */
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+  
+}
+
+
+const [,, searchLink] = process.argv;
 sandbox(searchLink);
+//sandbox22(searchLink);
+//const listoflinks=linksOfMichelin(searchLink);
+
+
